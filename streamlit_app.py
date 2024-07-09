fc3f14771c4d94b4993f8e5b69d90dcce03a640b
vp8888117 <viktorpahl@eaton.com>
2024-07-09 10:26:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ pd_df=my_dataframe.to_pandas()
 # st.dataframe(pd_df)
 # st.stop()
 
-ingredients_list = pd.multiselect(
+ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
     , max_selections=5
@@ -48,17 +48,16 @@ if ingredients_list:
     
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+                my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
     #st.write(my_insert_stmt)
 
     
-    time_to_insert = st.button('Submit Order')
-    
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!'+name_on_order, icon="✅")
+        time_to_insert = st.button('Submit Order')
+            if time_to_insert:
+            session.sql(my_insert_stmt).collect()
+            st.success('Your Smoothie is ordered!'+name_on_order, icon="✅")
 
 
 
