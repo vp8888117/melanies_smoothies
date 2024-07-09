358fc719950958719d4906bdd85afd5ee779f10c
vp8888117 <viktorpahl@eaton.com>
2024-07-09 09:58:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-##import snowflake-connector-python[pandas]
+import snowflake-connector-python[pandas]
 
 import pandas as pd
 
