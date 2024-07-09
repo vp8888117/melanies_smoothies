2599130d172b0a41c8773f7f4b9d37e76f46454a
vp8888117 <viktorpahl@eaton.com>
2024-07-09 10:23:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ pd_df=my_dataframe.to_pandas()
 # st.dataframe(pd_df)
 # st.stop()
 
-ingredients_list = st.multiselect(
+ingredients_list = pd.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
     , max_selections=5
